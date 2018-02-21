cba19a542ec0c7214327d70c1e675b024c9c92b5
WillB01 <wb.william@outlook.com>
2018-02-21 12:26:43 +0100

    adding stuff

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+           
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+
+
+    }
+}
---
+++ CityInfoAPI/Models/CitiesDataStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Models
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "Lorrem lorem lorem"
+
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "Lorrem lorem lorem"
+
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "Lorrem lorem lorem"
+
+                },
+            };
+        }
+    }
+}
