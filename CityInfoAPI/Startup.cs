2a4b505ee7058c2da5d25567f5124d7edf4ed542
WillB01 <wb.william@outlook.com>
2018-02-27 21:14:24 +0100

    adding stuff

--- CityInfoAPI/Startup.cs
+++ CityInfoAPI/Startup.cs
@@ -5,21 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CityInfoAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
         {
-            services.AddMvc();
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
+                //.AddJsonOptions(o => {
+                //    if(o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver 
+                //            as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null; 
+                           
+                //    }
+                //});
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -29,14 +51,11 @@ namespace CityInfoAPI
             {
                 app.UseExceptionHandler();
             }
-
+            
             app.UseStatusCodePages();
             app.UseMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
